e1d89022587b38e9cdf064d0be12451db7f015e9
shymaSane <shymaSane1@gmail.com>
2021-07-06 15:55:16 +0300

    restructuring project

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
+import 'react-native-gesture-handler';
 import React,{ Component } from 'react';
 import { Text,  View } from 'react-native';
-import Landing from './components/auth/Landing'
 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
 class App extends Component{
   render() {
     return(
-      <View>
-          <Landing />
-      </View>
+     <Text>Hello</Text>
     )
   }
+  
 }
 
 export default App; 
