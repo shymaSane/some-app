6585563ed3fcbda49527205019c161d77dd105f9
shymaSane <shymaSane1@gmail.com>
2021-09-04 20:24:18 +0300

    add: tab icon styling

--- src/app/navigation/TabNavigator.js
+++ src/app/navigation/TabNavigator.js
@@ -9,14 +9,21 @@ import {COLORS} from '../assets/colors';
 //tab bar
 export const TabIcon = ({navigation}) => {
   return (
-    <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-      <View>
+    <TouchableOpacity
+      onPress={() => navigation.navigate('Home')}
+      style={{
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
         <Image
           source={require('../assets/images/home-light.png')}
           resizeMode="contain"
-          style={{width: 30, height: 30, color: COLORS.decrease}}
+          style={{width: 25, height: 25, marginBottom: 2}}
         />
-        <Text>HOME</Text>
+        <Text style={{fontSize: 10}}>HOME</Text>
       </View>
     </TouchableOpacity>
   );
@@ -24,7 +31,7 @@ export const TabIcon = ({navigation}) => {
 
 const Tab = createBottomTabNavigator();
 
-export default TabNavigator = ({navigation}) => {
+export default TabNavigator = () => {
   return (
     <Tab.Navigator
       tabBarOptions={{
@@ -32,8 +39,8 @@ export default TabNavigator = ({navigation}) => {
         style: {
           position: 'absolute',
           height: 70,
-          borderTopWidth: 1,
-          borderTopColor: COLORS.primary,
+          // borderTopWidth: 1,
+          // borderTopColor: COLORS.primary,
           elevation: 0,
         },
       }}>
@@ -46,8 +53,24 @@ export default TabNavigator = ({navigation}) => {
           };
         }}
       />
-      <Tab.Screen name="Explore" component={ExploreScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Explore"
+        component={ExploreScreen}
+        options={({navigation}) => {
+          return {
+            tabBarIcon: () => <TabIcon navigation={navigation} />,
+          };
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={({navigation}) => {
+          return {
+            tabBarIcon: () => <TabIcon navigation={navigation} />,
+          };
+        }}
+      />
     </Tab.Navigator>
   );
 };
